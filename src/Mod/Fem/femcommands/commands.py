c51fe24c775ffd6756d4a516416ae6cc8fb36974
Stefan Tröger <stefantroeger@gmx.net>
2025-04-16 18:47:01 +0200

    Fem: Implement lineplot visualization

--- src/Mod/Fem/femcommands/commands.py
+++ src/Mod/Fem/femcommands/commands.py
@@ -1248,4 +1248,5 @@ FreeCADGui.addCommand("FEM_PostFilterGlyph", _PostFilterGlyph())
 
 # setup all visualization commands (register by importing)
 import femobjects.post_histogram
+import femobjects.post_lineplot
 post_visualization.setup_commands("FEM_PostVisualization")
